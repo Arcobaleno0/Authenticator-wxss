d2696f031de4ad152a183e387d125883f9ee57e2
zishang520 <zishang520@gmail.com>
2017-09-04 15:37:55 +0800

    First commit ‘

---
+++ pages/Input/Input.js
@@ -0,0 +1,74 @@
+var getData = function(k, v) {
+    return {
+        [k]: v
+    };
+};
+var TOOL = require('../../utils/index.js');
+Page({
+    data: {
+        usernamefocus: false,
+        usernameinput: false,
+        tokenfocus: false,
+        tokeninput: false,
+        array: ['基于时间', '基于计数器'],
+        index: 0,
+        errorMsg: "密钥值太短",
+    },
+    Inputfocus: function(e) {
+        this.setData(getData(e.target.id + 'focus', true));
+    },
+    Inputblur: function(e) {
+        this.setData(getData(e.target.id + 'focus', false));
+    },
+    Inputinput: function(e) {
+        if (e.target.id == 'token') {
+            if (!(/^[A-Za-z0-9]+($|=+$)/).test(e.detail.value)) {
+                this.setData({
+                    errorMsg: '密钥值中含有非法字符'
+                });
+                this.setData(getData('tokenerror', true));
+            } else {
+                this.setData(getData(e.target.id + 'error', false));
+            }
+        }
+        if (e.detail.value.length > 0) {
+            this.setData(getData(e.target.id + 'input', true));
+        } else {
+            this.setData(getData(e.target.id + 'input', false));
+        }
+    },
+    bindPickerChange: function(e) {
+        this.setData({
+            index: e.detail.value
+        })
+    },
+    formSubmit: function(e) {
+        if (e.detail.value.token.length < 16) {
+            this.setData({
+                errorMsg: '密钥值太短'
+            });
+            this.setData(getData('tokenerror', true));
+        }
+        var data = e.detail.value;
+        var obj = {
+          "secret": data.token,
+          "issuer": data.username,
+          "algorithm": "SHA1",
+          "digits": "6",
+          "period": "30",
+          "encoding": "base32",
+          "type": ((data.type >> 0) == 0) ? "totp" : 'hotp',
+          "label": ""
+        }
+        var data = wx.getStorageSync('DATA') || [];
+        data.push(obj);
+        // this.setData({
+        //   DATA: TOOL.parse_code(data)
+        // });
+        // canvas(wx.createCanvasContext('canvas' + (data.length - 1)));
+        wx.setStorageSync('DATA', data)
+        wx.redirectTo({
+          url: '../index/index',
+        });
+    }
+});
